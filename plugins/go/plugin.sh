982777496eb34198b5dce6d1afda14156055cb1c
Hao Su <hao.su@coinbase.com>
2019-12-09 11:02:26 -0800

    Update configs

--- plugins/go/plugin.sh
+++ plugins/go/plugin.sh
@@ -1,6 +1,7 @@
-export GOPATH=$HOME/Projects/go
+# export GOPATH=$HOME/Projects/go
 export GOBIN=$GOPATH/bin
-export GOROOT=/usr/local/opt/go/libexec
-export PATH=$PATH:$(go env GOPATH)/bin:$GOROOT/bin
-export GO111MODULE=on
+export GOROOT=/usr/local/go/
+export PATH=$PATH:$GOROOT/bin
+# export GO111MODULE=on
 export GOPROXY=https://gomodules.cbhq.net/
+export GOSUMDB=off
--- plugins/nvm/plugin.sh
+++ plugins/nvm/plugin.sh
@@ -1,2 +1,2 @@
 export NVM_DIR="$HOME/.nvm"
-alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' # This loads nvm
+alias loadnvm='[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm'
